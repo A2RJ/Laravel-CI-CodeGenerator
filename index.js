673c399c1ec287bb3aff66b8f6224936cf9ffd8c
ARDIANSYAH PUTRA - A2RJ <47985349+A2RJ@users.noreply.github.com>
2021-01-29 22:14:40 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Laravel = require('./src/laravel/index.js')
 
-new Laravel.models(); 
-// new Laravel.controller(); 
+// new Laravel.models(); 
+new Laravel.controller(); 
 
 // console.log(process.env.APP_PATH);
--- src/laravel/index.js
+++ src/laravel/index.js
@@ -5,15 +5,16 @@ const fields = require("../../config/config.json")
 const values = Object.values(fields.table)
 const keys = Object.keys(fields.table)
 
+function jsUcfirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 module.exports.models = function () {
-    let j = 0
-    for (const key of keys) {
-        fse.copy('src/laravel/app/app/Models/Models.php', process.env.APP_PATH + '/app/Models/' + keys[j] + 'Models.php', { overwrite: true }, err => {
+    for (let index = 0; index < keys.length; index++) {
+        fse.copy('src/laravel/app/app/Models/Models.php', process.env.APP_PATH + '/app/Models/' + keys[index] + 'Models.php', { overwrite: true }, err => {
             if (err) return console.error(err)
         })
-        j++
     }
-    
     setTimeout(() => {
         for (let k = 0; k < values.length; k++) {
             replace({
@@ -23,7 +24,7 @@ module.exports.models = function () {
                 recursive: true,
                 silent: true,
             });
-            
+
             replace({
                 regex: "TABLE",
                 replacement: keys[k],
@@ -43,9 +44,42 @@ module.exports.models = function () {
     }, 200);
 }
 
-// module.exports.controller = function () {
+module.exports.controller = function () {
 
-// }
+    for (let index = 0; index < keys.length; index++) {
+        fse.copy('src/laravel/app/app/Http/Controllers/Controllers.php', process.env.APP_PATH + '/app/Http/Controllers/' + jsUcfirst(keys[index]) + 'Controllers.php', { overwrite: true }, err => {
+            if (err) return console.error(err)
+        })
+    }
+
+    setTimeout(() => {
+        for (let k = 0; k < values.length; k++) {
+            replace({
+                regex: "FIELDS",
+                replacement: JSON.stringify(values[k]),
+                paths: [process.env.APP_PATH + '/app/Models/' + keys[k] + 'Models.php'],
+                recursive: true,
+                silent: true,
+            });
+
+            replace({
+                regex: "TABLE",
+                replacement: keys[k],
+                paths: [process.env.APP_PATH + '/app/Models/' + keys[k] + 'Models.php'],
+                recursive: true,
+                silent: true,
+            });
+
+            replace({
+                regex: "PRIMARY_KEY",
+                replacement: values[k][0],
+                paths: [process.env.APP_PATH + '/app/Models/' + keys[k] + 'Models.php'],
+                recursive: true,
+                silent: true,
+            });
+        }
+    }, 200);
+}
 
 // module.exports.view = function () {
 
